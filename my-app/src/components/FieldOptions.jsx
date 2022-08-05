4e5488234c7cf3ed99655f0416b7d969fa3dd4e0
Prashant Kumar <prashantkrmathur@gmail.com>
2022-08-05 15:43:07 +0530

    showing the data by clicking the random button

--- my-app/src/components/FieldOptions.jsx
+++ my-app/src/components/FieldOptions.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { city } from './constant/Location';
+import { UserName } from './constant/UserName';
+import {schoolData } from './constant/School'
 import "./FieldOptions.css";
+import { MajorData } from './constant/Major';
+import { occupationData } from './constant/Occupation';
+import { religiousViews } from './constant/ReligiousViews';
+import {reasonData} from "./constant/Reason.js"
 
 const FieldOptions = (props) => {
   const { name, setName, gender, setGender, location, setLocation, school, setSchool, major, setMajor, occupation, setOccupation, religiousBackground, setReligiousBackground, reason, setReason, isLocation, setIsLocation, isSchool, setIsSchool, isOccupation, setIsOccupation, isReligious, setIsReligious, isReason, setIsReason } = props
-  console.log("reason", reason);
 
   const handleNameChange = (event) => {
     event.preventDefault();
@@ -40,13 +46,60 @@ const FieldOptions = (props) => {
   function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-
   const rndId = randomIntFromInterval(0, 1000);
-
+  const handleRandomName = () => {
+    UserName.map((el) => {
+      if (rndId === el.id) {
+        setName(el.first_name + " " + el.last_name);
+        setGender(el.gender)
+      }
+    })
+  }
+  const handleRandomLocation = () => {
+    city.map((el) => {
+      if (rndId === el.id) {
+        setLocation(el.location)
+      }
+    })
+  }
+  const handleRandomSchool = () => {
+    schoolData.map((el) => {
+      if (rndId === el.id) {
+        setSchool(el.school)
+      }
+    })
+  }
+  const handleRandomMajor = () => {
+    MajorData.map((el) => {
+      if (rndId === el.id) {
+        setMajor(el.major)
+      }
+    })
+  }
+  const handleRandomOccupation = () => {
+    occupationData.map((el) => {
+      if (rndId === el.id) {
+        setOccupation(el.profession);
+      }
+    })
+  }
+  const handleReligiousView = () => {
+    religiousViews.map((el) => {
+      if (rndId === el.id) {
+        setReligiousBackground(el.religionView);
+      }
+    })
+  }
+  const handleRandomReason = () => {
+    reasonData.map((el) => {
+      if (rndId === el.id) {
+        setReason(el.religionView);
+      }
+    })
+  }
   return (
     <div className='settings'>
-      <h2>Options</h2>
-      <form>
+      <h2>Options</h2>     
         <div className='bio-element'>
           <label for='person-name-textbox'>Name</label>
           <input type='text' name='person-name-textbox' onChange={handleNameChange} value={name} ></input>
@@ -55,48 +108,47 @@ const FieldOptions = (props) => {
               <option>Male</option>
               <option>Female</option>
             </select>
-          <button>Random Name</button>
+          <button onClick={handleRandomName}>Random Name</button>
         </div>
         <div className='bio-element'>
           <input type="checkbox" id="location" name="location" onChange={() => setIsLocation(!isLocation)} checked={isLocation}></input>
           <label for="location">Location</label>
           <input type="text" className="location-textbox" onChange={handleLocation} value={location}></input>
-          <button>Random Location</button>
+          <button onClick={handleRandomLocation}>Random Location</button>
         </div>
         <div className='bio-element'>
           <input type="checkbox" id="school" name="school" onChange={() => setIsSchool(!isSchool)} checked={isSchool}></input>
           <label for="school">School</label>
           <input type="text" className="school-textbox" onChange={handleSchool} value={school}></input>
-          <button>Random School</button>
+          <button onClick={handleRandomSchool}>Random School</button>
           <br></br>
           <label for="degree-textbox">Major</label>
           <input type="text" name="degree-textbox" onChange={handleMajor} value={major}></input>
-          <button>Random Major</button>
+        <button onClick={handleRandomMajor}>Random Major</button>
         </div>
         <div className="bio-element">
           <input type="checkbox" id="occupation" name="occupation" onChange={() => setIsOccupation(!isOccupation)} checked={isOccupation} ></input>
           <label for="occupation">Occupation</label>
           <input type="text" name="occupation-textbox" onChange={handleOccupation} value={occupation}></input>
-          <button>Random Occupation</button>
+          <button onClick={handleRandomOccupation}>Random Occupation</button>
         </div>
         <div className="bio-element">
           <input type="checkbox" id="religion" name="religion" onChange={() => setIsReligious(!isReligious) } checked={isReligious}></input>
           <label for="religion">Religious Background</label>
           <textarea type="text" name="religion-textbox" onChange={handleReligion} style={{ width: '525px', height: '100px' }} value={religiousBackground}></textarea>
-          <button>Random Religion</button>
+          <button onClick={handleReligiousView}>Random Religion</button>
         </div>
         <div className="bio-element">
           <input type="checkbox" id="reason" name="reason" onChange={() => setIsReason(!isReason)} checked={isReason}></input>
           <label for="desire"> Reason for meeting with missionaries </label>
           <textarea type="text" name="desire-textbox" onChange={handleReason}n style={{width:'525px', height:'100px'}} value={reason} ></textarea>
-          <button className="restoration">Restoration</button>
-          <button className="plan-of-salvation">Plan of Salvation</button>
-          <button className="gospel-of-christ">Gospel of Christ</button>
-          <button className="law-of-chastity">Law of Chastity</button>
-          <button className="word-of-wisdom">Word of Wisdom</button>
-          <button>Any Lesson</button>
+          <button className="restoration" onClick={handleRandomReason}>Restoration</button>
+        <button className="plan-of-salvation" onClick={handleRandomReason}>Plan of Salvation</button>
+        <button className="gospel-of-christ" onClick={handleRandomReason}>Gospel of Christ</button>
+        <button className="law-of-chastity" onClick={handleRandomReason}>Law of Chastity</button>
+        <button className="word-of-wisdom" onClick={handleRandomReason}>Word of Wisdom</button>
+        <button onClick={handleRandomReason}>Any Lesson</button>
           </div>
-      </form>
     </div>
   )
 }
